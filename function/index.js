9e899512de1a7998ee36229a99bed9eecd50abe9
Alireza Ghasemie <alireza8ghasemi3@gmail.comm>
2023-08-29 18:26:23 +0330

    4

---
+++ function/index.js
@@ -0,0 +1,39 @@
+function calculate(a,b){
+    return (a +b)
+};
+
+
+
+
+function calculate(a ,b){
+    return (a -b)
+};
+
+
+
+function calculate(a ,b){
+     return (a *b)
+ };
+
+
+
+ function calculate(a ,b){
+     return (a /b).toFixed(2)
+ }
+
+
+
+ function sum(num) {
+    if (num === 1) {
+        return 1;
+    } else {
+        return num + sum(num - 1)
+    }
+}
+alert(sum(4))
+
+
+
+
+
+  
